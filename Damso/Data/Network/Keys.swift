850f9079bad095340696f9e1862b541b9da2aa10
J.H. Moon <moonjh587@naver.com>
2023-02-06 23:26:13 +0900

    [Refactor] - #28 API Key 숨기기 작업

---
+++ Damso/Data/Network/Keys.swift
@@ -0,0 +1,15 @@
+//
+//  Keys.swift
+//  Damso
+//
+//  Created by 문정호 on 2023/02/06.
+//
+
+import Foundation
+
+
+class Keys{
+    static let naverClientID = "srlrxzdlgz"
+    
+    static let naverClientSecret = "ZORKnOFGQPuharNZ4avXe8Ggt7ymU7CTMZeD4lJc"
+}
