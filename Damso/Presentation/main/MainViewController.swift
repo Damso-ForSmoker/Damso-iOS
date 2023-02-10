e1fff17227bd4ba219f3552b0d2214768f6747cb
ae-chae <aechae11@gmail.com>
2023-02-10 11:51:57 +0900

    Scene/#17

--- Damso/Presentation/main/MainViewController.swift
+++ Damso/Presentation/main/MainViewController.swift
@@ -15,6 +15,8 @@ import Alamofire
 class MainViewController: UIViewController, UISheetPresentationControllerDelegate, CLLocationManagerDelegate{
     
     var locationManager = CLLocationManager()
+    lazy var pinArr: Array<Pin> = []
+    lazy var markerArr: Array<NMFMarker> = []
     
     
     override func viewDidLoad() {
@@ -30,35 +32,49 @@ class MainViewController: UIViewController, UISheetPresentationControllerDelegat
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
-                
+        
         if CLLocationManager.locationServicesEnabled() {
             print("위치 서비스 On 상태")
             locationManager.startUpdatingLocation()
             print(locationManager.location?.coordinate)
-                    
-                    //현 위치로 카메라 이동
+            
+            //현 위치로 카메라 이동
             let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0))
             cameraUpdate.animation = .easeIn
             naverMapView.mapView.moveCamera(cameraUpdate)
-                    
-                } else {
-                    print("위치 서비스 Off 상태")
+            
+        } else {
+            print("위치 서비스 Off 상태")
+        }
+        
+        //        var marker = NMFMarker()
+        //        marker.position = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
+        //        marker.mapView = mapView
+        
+        //let markersHTTPMethod = MarkersHTTPMethod()
+        
+        //private init() {}
+        
+        
+        //marker.iconImage = NMFOverlayImage(name: "marker_icon")
+        
+        for i in 0...9 {
+            MarkersHTTPMethod.markersHTTPMethod.callPin() { pinInfoArr in
+                DispatchQueue.main.async {
+                    self.pinArr = pinInfoArr
+                    //                    print(self.pinArr)
+                    self.markerArr.append(NMFMarker(position: NMGLatLng(lat: Double(pinInfoArr[i].la)!, lng: Double(pinInfoArr[i].lo)!)))
+                    self.markerArr[self.markerArr.count-1].mapView = naverMapView.mapView
                 }
-        
-//        var marker = NMFMarker()
-//        marker.position = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
-//        marker.mapView = mapView
-        
-        
-        MarkersHTTPMethod.markersHTTPMethod.callPin{ marker in
-            marker.mapView = naverMapView.mapView
+            }
+            print(self.pinArr)
+            
+            
+            //            markerArr.append(NMFMarker(position: NMGLatLng(lat: Double(pinArr[i].la)!, lng: Double(pinArr[i].lo)!)))
+            //            markerArr[i].mapView = naverMapView.mapView
         }
     }
     
-
     
     
 }
-
-
-
