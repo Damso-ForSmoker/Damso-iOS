6cf6eb1dee14ac633f27144a33a2a582e379d92f
J.H. Moon <moonjh587@naver.com>
2023-02-15 08:29:02 +0900

    [Feat] - # 37 제보하기 서버 연동 에러 처리

--- Damso/Data/Network/ReportInput/ReportInputResult.swift
+++ Damso/Data/Network/ReportInput/ReportInputResult.swift
@@ -11,4 +11,17 @@ struct ReportInputResult: Codable{
     var isSuccess: Bool
     var code: Int
     var message: String
+    
+    init(from decoder: Decoder) throws { //message가 boolean으로 들어올 경우가 있어서 디코더 throw를 해주어야 함.
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        isSuccess = try container.decode(Bool.self, forKey: .isSuccess)
+        code = try container.decode(Int.self, forKey: .code)
+        do{
+            message = try container.decode(String.self, forKey: .message)
+        } catch{
+            let booleanInput = try container.decode(Bool.self, forKey: .message)
+            message = String(booleanInput)
+        }
+    }
 }
