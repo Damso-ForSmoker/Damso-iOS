549bf965e4c933ce4d8d3b03aace117ac16c27fa
ae-chae <aechae11@gmail.com>
2023-02-15 22:59:57 +0900

    Scene /#17

--- Damso/Presentation/main/BottomSheetViewController2.swift
+++ Damso/Presentation/main/BottomSheetViewController2.swift
@@ -5,46 +5,54 @@
 //  Created by 윤채영 on 2023/01/31.
 //
 
-import Foundation
-
 import UIKit
-// ReportPushViewController
+import NMapsMap
+import CoreLocation
+import Alamofire
+
 class BottomSheetViewController2: UIViewController {
 
-    @IBAction func reportPush(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let bottomSheetVC = storyboard.instantiateViewController(identifier: "ReportPushViewController")
-            // 1
-        //let bottomSheet: MDCBottomSheetController  = MDCBottomSheetController(contentViewController: bottomSheetVC)
-        
-        if let sheet = bottomSheetVC.sheetPresentationController {
-          // customize
-            sheet.detents = [
-                    .custom { context in
-                        return context.maximumDetentValue * 0.4
-                    }
-                ]
-            //sheet.largestUndimmedDetentIdentifier = .medium
-            sheet.preferredCornerRadius = 20
-        }
-        
-        self.present(bottomSheetVC, animated: false, completion: nil)
-    }
+    var locationManager = CLLocationManager()
+    lazy var naverMapView = NMFNaverMapView()
     
-    @IBOutlet weak var infoView: UIView!
-    @IBOutlet weak var reviewView: UIView!
+//    @IBAction func reportPush(_ sender: Any) {
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let bottomSheetVC = storyboard.instantiateViewController(identifier: "ReportPushViewController")
+//            // 1
+//        //let bottomSheet: MDCBottomSheetController  = MDCBottomSheetController(contentViewController: bottomSheetVC)
+//
+//        if let sheet = bottomSheetVC.sheetPresentationController {
+//          // customize
+//            sheet.detents = [
+//                    .custom { context in
+//                        return context.maximumDetentValue * 0.4
+//                    }
+//                ]
+//            //sheet.largestUndimmedDetentIdentifier = .medium
+//            sheet.preferredCornerRadius = 20
+//        }
+//
+//        self.present(bottomSheetVC, animated: false, completion: nil)
+//    }
+    
+    @IBOutlet weak var subMapView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        infoView.layer.shadowOffset = CGSize(width: 10, height: 10)
-        infoView.layer.shadowOpacity = 0.5
-        infoView.layer.shadowRadius = 10
-        infoView.layer.cornerRadius = 10
         
-        reviewView.layer.shadowOffset = CGSize(width: 10, height: 10)
-        reviewView.layer.shadowOpacity = 0.5
-        reviewView.layer.shadowRadius = 10
-        reviewView.layer.cornerRadius = 10
+        naverMapView = NMFNaverMapView(frame: view.frame)
+        naverMapView.showLocationButton = true
+        subMapView.addSubview(naverMapView)
+        
+        naverMapView.mapView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
+        naverMapView.mapView.leadingAnchor.constraint(equalTo: self.subMapView.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true //대신 하단은 safeAreaLayoutGuide에 맞춰서
+        
+        
+        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? Double(37.545251), lng: locationManager.location?.coordinate.longitude ?? Double(126.952514))) // 선택 마커 위치로 시점 이동
+        cameraUpdate.animation = .easeIn
+        naverMapView.mapView.moveCamera(cameraUpdate)
+        
+   
     }
     
 }
